4919d2fa1a31385fa4826c0fc896d835252dca6f
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-07 18:35:13 +0530

    Create Solution.py

---
+++ A. Bank and its Cashier/Solution.py
@@ -0,0 +1,12 @@
+### KNOWLEDGE IS POWER  ###
+
+def sol(n):
+   #add code here
+
+# do not edit below code
+def main():
+    n=int(input())
+    print(sol(n))
+
+if __name__ == '__main__':
+    main()
