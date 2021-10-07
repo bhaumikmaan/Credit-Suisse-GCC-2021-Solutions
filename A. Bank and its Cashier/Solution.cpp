900c69ab48092fb431cc37ff8a707c0c94bdf6b7
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-07 18:36:20 +0530

    Create Solution.cpp

---
+++ A. Bank and its Cashier/Solution.cpp
@@ -0,0 +1,18 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+
+using namespace std;
+#include <string>
+#include <iostream>
+
+int solution(int n){
+    // Solution Here
+}
+
+/*  Do not edit below code */
+int main() {
+	int n;
+	cin >> n;	
+        int answer=solution(n);
+	cout << answer << endl;	
+}
