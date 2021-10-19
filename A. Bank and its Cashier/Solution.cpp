af2bda487615c090f79fe3b45ed3afe32e7c6690
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-19 13:26:23 +0530

    Update Solution.cpp

--- A. Bank and its Cashier/Solution.cpp
+++ A. Bank and its Cashier/Solution.cpp
@@ -1,18 +1,26 @@
 /***  KNOWLEDGE IS POWER  ***/
 
-
 using namespace std;
 #include <string>
 #include <iostream>
 
 int solution(int n){
-    // Solution Here
+    int x ;
+    if(n%3 == 0)
+    {
+    	x = n/3 + n/3 ;
+    }
+    else
+    {
+    	x = (n/3 + 1) + n/3 ;
+    }
+    return x ;
 }
 
 /*  Do not edit below code */
 int main() {
 	int n;
 	cin >> n;	
-        int answer=solution(n);
+    int answer=solution(n);
 	cout << answer << endl;	
 }
